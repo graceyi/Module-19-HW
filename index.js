5184fa3df51a180171549502fb9bcc046e4c153a
graceyi <grace.yi@du.edu>
2022-10-31 12:57:33 -0600

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+ScrollReveal({ duration: 1000 })
+
+// ScrollReveal().reveal('.navigation', { delay: 300 });
+
+// ScrollReveal().reveal('.heroImage', { delay: 400 });
+
+ScrollReveal().reveal('.mySkills', { delay: 400 });
+
+ScrollReveal().reveal('.userResearch', { delay: 600 });
+
+ScrollReveal().reveal('.collab', { delay: 800 });
+
+ScrollReveal().reveal('.designStrat', { delay: 1000 });
+
+// ScrollReveal().reveal('.flexContainer', { delay: 500 });
+
+ScrollReveal().reveal('.myWork', { delay: 500 });
+
+ScrollReveal().reveal('.webRedesign', { delay: 600 });
+
+ScrollReveal().reveal('.govRedesign', { delay: 700 });
+
+ScrollReveal().reveal('.mobileProto', { delay: 800 });
+
+// ScrollReveal().reveal('.workGrid', { delay: 500 });
+
+// ScrollReveal().reveal('.footer', { delay: 300 }); 
+
